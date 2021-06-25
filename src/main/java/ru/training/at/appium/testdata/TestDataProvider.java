3df7645cf34b54f15b021c14cd1c354f5658abb6
daxdue <s.y.kochetkov@gmail.com>
2021-06-25 16:45:17 +0300

    Added JSON test data source

--- src/main/java/ru/training/at/appium/testdata/TestDataProvider.java
+++ src/main/java/ru/training/at/appium/testdata/TestDataProvider.java
@@ -2,12 +2,18 @@ package ru.training.at.appium.testdata;
 
 import org.testng.annotations.DataProvider;
 import ru.training.at.appium.constants.DataProviders;
+import ru.training.at.appium.models.TestObject;
+import ru.training.at.appium.utils.TestDataLoader;
+
+import java.util.List;
 
 public class TestDataProvider {
 
     @DataProvider(name = DataProviders.NATIVE_TEST_DATA)
     public Object[][] accountTestData() {
-        return new Object[][]{{"test@test.ru", "test",
-                "qzwxecasd123", "Budget"}};
+        List<TestObject> testObjectList = TestDataLoader.load();
+        return testObjectList.stream()
+                .map(data -> new TestObject[]{data})
+                .toArray(Object[][]::new);
     }
 }
--- src/test/java/ru/training/at/appium/nativeapp/NativeAppTests.java
+++ src/test/java/ru/training/at/appium/nativeapp/NativeAppTests.java
@@ -5,32 +5,32 @@ import org.testng.annotations.Test;
 import ru.training.at.appium.BaseTest;
 import ru.training.at.appium.constants.DataProviders;
 import ru.training.at.appium.enums.SwipeDirection;
+import ru.training.at.appium.models.TestObject;
 import ru.training.at.appium.testdata.TestDataProvider;
 
 public class NativeAppTests extends BaseTest {
 
     @Test(dataProvider = DataProviders.NATIVE_TEST_DATA, dataProviderClass = TestDataProvider.class)
-    public void signinAppTest(String testEmail, String testLogin,
-                              String testPassword, String requiredActivityName) {
+    public void signinAppTest(TestObject testObject) {
         try {
             //Open account registration screen
             pageObject.getPageElement("registerButton").click();
             //Input email for new account
             pageObject.getPageElement("registerEmailInput").click();
             pageObject.getPageElement("registerEmailInput")
-                    .sendKeys(testEmail);
+                    .sendKeys(testObject.getEmail());
             //Input username for new account
             pageObject.getPageElement("registerUsernameInput").click();
             pageObject.getPageElement("registerUsernameInput")
-                    .sendKeys(testLogin);
+                    .sendKeys(testObject.getLogin());
             //Input password for new account
             pageObject.getPageElement("registerPasswordInput").click();
             pageObject.getPageElement("registerPasswordInput")
-                    .sendKeys(testPassword);
+                    .sendKeys(testObject.getPasswordConfirmation());
             //Confirm password
             pageObject.getPageElement("registerConfirmPasswordInput").click();
             pageObject.getPageElement("registerConfirmPasswordInput")
-                    .sendKeys(testPassword);
+                    .sendKeys(testObject.getPasswordConfirmation());
             //Hide keyboard
             //appiumDriver.hideKeyboard();
             swipeScreen(SwipeDirection.DOWN);
@@ -39,16 +39,16 @@ public class NativeAppTests extends BaseTest {
                     .click();
             //Input account email
             pageObject.getPageElement("loginEmailInput")
-                    .sendKeys(testEmail);
+                    .sendKeys(testObject.getEmail());
             //Input account password
             pageObject.getPageElement("passwordInput")
-                    .sendKeys(testPassword);
+                    .sendKeys(testObject.getPassword());
             //Perform login
             pageObject.getPageElement("signInButton")
                     .click();
             String activityName = pageObject.getPageElement("activityName").getText();
             //Check current screen name
-            Assert.assertTrue(activityName.contains(requiredActivityName));
+            Assert.assertTrue(activityName.contains(testObject.getActivityName()));
 
         } catch (Exception e) {
             e.printStackTrace();
